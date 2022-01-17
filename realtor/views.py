6e87bd4b1ee173cfa46e337071e9c3d397cac5f3
cavidparker <cavidpark@gmail.com>
2022-01-17 13:48:53 +0600

    change view in realtor

--- realtor/views.py
+++ realtor/views.py
@@ -7,22 +7,26 @@ from rest_framework import permissions
 
 # Create your views here.
 
+#### get all agents ####
 class AgentListView(ListAPIView):
     permission_classes = (permissions.AllowAny,)
     queryset = Agent.objects.all()
     serializer_class = AgentSerializer
     pagination_class = None
 
+##### single object #####
 class AgentDetailView(RetrieveAPIView):
     queryset = Agent.objects.all()
     serializer_class = AgentSerializer
 
+#### Top seller list ####
 class TopSellerListView(ListAPIView):
     permission_classes = (permissions.AllowAny,)
     queryset = Agent.objects.filter(top_seller=True)
     serializer_class = AgentSerializer
     pagination_class = None
 
+#### post #####
 class CreateApiView(ListCreateAPIView):
     permission_classes = (permissions.AllowAny,)
     queryset = Agent.objects.all()
